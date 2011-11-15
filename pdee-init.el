c6005ed04ba6457eaa01c01ef94ace58d6276093
Andreas Roehler <andreas.roehler@online.de>
2011-11-15 13:22:00 +0100

    pdee-setup.el removed, let pdee-init.el do all

--- pdee-init.el
+++ pdee-init.el
@@ -2,8 +2,31 @@
 
 ;; Trick to get the filename of the installation directory
 
+(defgroup python nil
+  "Python Language's support for Emacs."
+  :group 'languages)
+
+(defcustom pdee-install-dir nil
+  "Directory where PDEE should be installed. Must end with a slash. "
+  :type 'string
+  :group 'python)
+
+(unless pdee-install-dir
+  (message "pdee-install-dir %s" (concat "not found, use " (expand-file-name "~/")))
+  (setq pdee-install-dir (concat (expand-file-name "~/") "pdee")))
+
 (add-to-list 'load-path pdee-install-dir)
-(require 'pdee-setup)
+
+;; Adding paths to the variable load-path
+(dolist (relpath '(""
+                   "extensions/"
+                   "extensions/yasnippet"
+                   "extensions/auto-complete"
+		   "extensions/eproject"
+                   )
+                 )
+  (add-to-list 'load-path (concat pdee-install-dir relpath)))
+
 (require 'pdee-python)
 (require 'pdee-completion)
 (require 'pdee-editing)
