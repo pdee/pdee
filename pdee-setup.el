fcae9287c97659c4dd094eee1f9334b938618206
Andreas Roehler <andreas.roehler@online.de>
2011-11-15 11:41:52 +0100

    pdee-install-dir default, made customizable
    install may start with a file-less buffer, (buffer-file-name) will fail then

--- pdee-setup.el
+++ pdee-setup.el
@@ -1,11 +1,23 @@
 ;; pdee-setup.el - setup and load all the paths necessary
 
-(defconst pdee-install-dir
-  (file-name-directory (or load-file-name
-                           (when (boundp 'bytecomp-filename) bytecomp-filename)
-                           buffer-file-name))
-  "Installation directory of emacs-for-python terminating with a slash"
-  )
+(defgroup python nil
+  "Python Language's support for Emacs."
+  :group 'languages)
+
+(defcustom pdee-install-dir nil
+  "Directory where PDEE should be installed. "
+  :type 'string
+  :group 'python)
+
+(unless pdee-install-dir
+  (setq pdee-install-dir (expand-file-name "~/")))
+
+;; (defconst pdee-install-dir
+;;   (file-name-directory (or load-file-name
+;;                            (when (boundp 'bytecomp-filename) bytecomp-filename)
+;;                            buffer-file-name))
+;;   "Installation directory of emacs-for-python terminating with a slash"
+;;   )
 
 
 ;; Adding paths to the variable load-path
