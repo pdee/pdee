d96a812883aea79eee1c3ee252a40ed4ace7d3d7
Andreas Roehler <andreas.roehler@online.de>
2011-12-10 09:46:00 +0100

    pdee-python.el merged into pdee-init.el

--- pdee-init.el
+++ pdee-init.el
@@ -93,4 +93,132 @@ When called without arguments, default mode is switched on. "
   (require 'pdee-editing)
   (require 'pdee-bindings))
 
+;;; Pymacs
+(require 'pymacs (concat pdee-install-dir "extensions/pymacs.el"))
+
+(defun setup-ropemacs ()
+  "Setup the ropemacs harness"
+  (setenv "PYTHONPATH"
+          (concat
+           (getenv "PYTHONPATH") path-separator
+           (concat (expand-file-name pdee-install-dir) "python-libs/")))
+  (message (getenv "PYTHONPATH"))
+  (pymacs-load "ropemacs" "rope-")
+
+  ;; Stops from erroring if there's a syntax err
+  (setq ropemacs-codeassist-maxfixes 3)
+
+  ;; Configurations
+  (setq ropemacs-guess-project t)
+  (setq ropemacs-enable-autoimport t)
+
+
+  (setq ropemacs-autoimport-modules '("os" "shutil" "sys" "logging"
+				      "django.*"))
+
+
+
+  ;; Adding hook to automatically open a rope project if there is one
+  ;; in the current or in the upper level directory
+  (add-hook 'python-mode-hook
+            (lambda ()
+              (cond ((file-exists-p ".ropeproject")
+                     (rope-open-project default-directory))
+                    ((file-exists-p "../.ropeproject")
+                     (rope-open-project (concat default-directory "..")))
+                    )))
+  )
+
+;;; Ipython integration
+(defun pdee-setup-ipython ()
+  "Setup ipython integration with python-mode"
+  (interactive)
+  (setq
+   python-shell-interpreter "ipython"
+   python-shell-interpreter-args ""
+   python-shell-prompt-regexp "In \[[0-9]+\]: "
+   python-shell-prompt-output-regexp "Out\[[0-9]+\]: "
+   python-shell-completion-setup-code ""
+   python-shell-completion-string-code "';'.join(__IP.complete('''%s'''))\n")
+  )
+
+;;; Flymake additions, I have to put this one somwhere else?
+
+
+(defun flymake-create-copy-file ()
+  "Create a copy local file"
+  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+                     'flymake-create-temp-inplace)))
+    (file-relative-name
+     temp-file
+     (file-name-directory buffer-file-name))))
+
+(defun flymake-command-parse (cmdline)
+  "Parses the command line CMDLINE in a format compatible
+       with flymake, as:(list cmd-name arg-list)
+
+The CMDLINE should be something like:
+
+ flymake %f python custom.py %f
+
+%f will be substituted with a temporary copy of the file that is
+ currently being checked.
+"
+  (let ((cmdline-subst (replace-regexp-in-string "%f" (flymake-create-copy-file) cmdline)))
+    (setq cmdline-subst (split-string-and-unquote cmdline-subst))
+    (list (first cmdline-subst) (rest cmdline-subst))
+    ))
+
+
+(when (load-file (concat pdee-install-dir "extensions/flymake-patch.el"))
+  (setq flymake-info-line-regex
+        (append flymake-info-line-regex '("unused$" "^redefinition" "used$")))
+  (load-library "flymake-cursor"))
+
+(defun pdee-setup-checker (cmdline)
+  (add-to-list 'flymake-allowed-file-name-masks
+               (list "\\.py\\'" (apply-partially 'flymake-command-parse cmdline)))
+  )
+
+
+;; Python or python mode?
+(eval-after-load 'python
+  '(progn
+     ;;==================================================
+     ;; Ropemacs Configuration
+     ;;==================================================
+     (setup-ropemacs)
+
+     ;;==================================================
+     ;; Virtualenv Commands
+     ;;==================================================
+     (autoload 'virtualenv-activate "virtualenv"
+       "Activate a Virtual Environment specified by PATH" t)
+     (autoload 'virtualenv-workon "virtualenv"
+       "Activate a Virtual Environment present using virtualenvwrapper" t)
+
+
+     ;; Not on all modes, please
+     (add-hook 'python-mode-hook 'flymake-find-file-hook)
+
+
+     )
+  )
+;;; Cython Mode
+(autoload 'cython-mode "cython-mode" "Mode for editing Cython source files")
+
+(add-to-list 'auto-mode-alist '("\\.pyx\\'" . cython-mode))
+(add-to-list 'auto-mode-alist '("\\.pxd\\'" . cython-mode))
+(add-to-list 'auto-mode-alist '("\\.pxi\\'" . cython-mode))
+
+;; Py3 files
+(add-to-list 'auto-mode-alist '("\\.py3\\'" . python-mode))
+
+(add-hook 'python-mode-hook
+          '(lambda ()
+             (cond ((boundp 'python-mode-map)
+                    (define-key python-mode-map "\C-m" 'newline-and-indent))
+                   ((boundp 'py-mode-map)
+                    (define-key py-mode-map "\C-m" 'newline-and-indent)))))
+
 (provide 'pdee-init)
