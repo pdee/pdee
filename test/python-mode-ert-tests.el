af5295d2c401b3187de7a2b8335ccdd11557a42c
Andreas Röhler <andreas.roehler@online.de>
2014-03-02 18:18:47 +0100

    `py-kill-buffer-unconditional', new command
     Kill buffer unconditional; if buffer-process exists, kill it first.
     Sometimes being queried sucks...
    
     Used with tests here

--- test/python-mode-ert-tests.el
+++ test/python-mode-ert-tests.el
@@ -623,22 +623,25 @@ with file(\"roulette-\" + zeit + \".csv\", 'w') as datei:
 (ert-deftest socket-modul-completion-lp-1284141 ()
   (py-tests-with-temp-buffer
       "import socket"
-    (py-execute-buffer-dedicated)
-    (set-buffer (py--fetch-first-python-buffer))(goto-char (point-min))
-    ;;    (sit-for 0.1)
-    ;;    (switch-to-buffer (current-buffer))
-    (goto-char (point-max))
-    (insert "socket.")
-    (py-shell-complete)
-    (set-buffer "*Python Completions*")
-    ;;    (sit-for 0.1)
-    ;;    (switch-to-buffer (current-buffer))
-    (goto-char (point-min))
-    (should (search-forward "socket."))))
+    (let (oldbuf)
+      (py-execute-buffer-dedicated)
+      (set-buffer (py--fetch-first-python-buffer))(goto-char (point-min))
+      (setq oldbuf (current-buffer))
+      (goto-char (point-max))
+      (insert "socket.")
+      (py-shell-complete)
+      (set-buffer "*Python Completions*")
+      (goto-char (point-min))
+      (sit-for 0.1) 
+      (and (should (search-forward "socket."))
+	   (py-kill-buffer-unconditional oldbuf)))))
 
 ;; (ert-deftest py-execute-statement-test ()
 ;;  (py-tests-with-temp-buffer
-;;      
-;;  )
-  
+;;      "print(\"I'm the py-execute-statement-test\")"
+
+
+;;  ))
+
 (provide 'python-mode-ert-tests)
+
