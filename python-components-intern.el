d2a87932760981a16e74d14653c9297cde7978a3
Andreas Röhler <andreas.roehler@online.de>
2014-06-03 08:00:25 +0200

    py--send-string-...
    Filter output only if non-empty string

--- python-components-intern.el
+++ python-components-intern.el
@@ -1190,7 +1190,7 @@ the output."
                       "")))))
     (py-shell-send-string string process msg)
     (accept-process-output process 5)
-    (when output
+    (when (and output (not (string= "" output)))
       (replace-regexp-in-string
        (if (> (length py-shell-prompt-output-regexp) 0)
            (format "\n*%s$\\|^%s\\|\n$"
@@ -1215,7 +1215,7 @@ the output."
                       "")))))
     (py-shell-send-string string process msg)
     (accept-process-output process 5)
-    (when output
+    (when (and output (not (string= "" output)))
       (setq output
             (replace-regexp-in-string
              (if (> (length py-shell-prompt-output-regexp) 0)
