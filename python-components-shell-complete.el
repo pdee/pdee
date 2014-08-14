5116d7e80fdd7a43020cfa915473a392c95cd6eb
Andreas Röhler <andreas.roehler@online.de>
2014-08-14 08:55:26 +0200

    `py-shell-complete': keep current buffer

--- python-components-shell-complete.el
+++ python-components-shell-complete.el
@@ -128,8 +128,9 @@ completions on the current context."
 (defun py-shell-complete (&optional shell debug beg end word)
   "Complete word before point, if any. "
   (interactive)
-  (and (buffer-live-p (get-buffer "*Python Completions*"))
-       (py-kill-buffer-unconditional "*Python Completions*"))
+  (save-excursion
+    (and (buffer-live-p (get-buffer "*Python Completions*"))
+	 (py-kill-buffer-unconditional "*Python Completions*")))
   (setq py-completion-last-window-configuration
         (current-window-configuration))
   (when debug (setq py-shell-complete-debug nil))
