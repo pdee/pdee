852745c3521cd0b8504e0a94383514b4471025a9
Andreas Röhler <andreas.roehler@online.de>
2014-09-21 21:19:15 +0200

    `py--execute-file-base' fixed
    `py-store-result-test' added

--- test/py-bug-numbered-tests.el
+++ test/py-bug-numbered-tests.el
@@ -5480,7 +5480,7 @@ def foo():
 
 \"\"\"Some docstring.\"\"\"
 
-__version__ = \"$Revision: 1.32 $\"
+__version__ = \"$Revision: 1.33 $\"
 
 "))
   (py-bug-tests-intern 'python-mode-very-slow-lp-1107037-base arg teststring)))
