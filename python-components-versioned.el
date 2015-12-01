e8fa04a043293d5cb0ea992aebd7d68bb88541a8
Andreas Röhler <andreas.roehler@online.de>
2015-12-01 16:10:09 +0100

    Urs Fleisch 2015-12-01 Fix pycomplete.py for Python 3

--- python-components-versioned.el
+++ python-components-versioned.el
@@ -17,7 +17,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-;;; Commentary:
+;;; Commentary: Highlight keywords according to Python version 2 or 3
 
 ;;
 
