956aaa13c89979ad96b4294ee0b19afcbf9b94b9
Andreas Roehler <andreas.roehler@online.de>
2019-09-11 20:43:31 +0200

    ‘py-execute-buffer’ fixed

--- python-components-extended-executes.el
+++ python-components-extended-executes.el
@@ -595,6 +595,7 @@ Keep current buffer. Ignores ‘py-switch-buffers-on-execute-p’ "
   (interactive)
   (let ((py-master-file (or py-master-file (py-fetch-py-master-file)))
         (wholebuf t)
+	(shell (py-choose-shell))
         filename buffer)
     (when py-master-file
       (setq filename (expand-file-name py-master-file)
