eacdfa42eac1993d7a7ad23e8cf63b0658b0fc73
Andreas Roehler <andreas.roehler@online.de>
2020-12-16 21:41:32 +0100

    code using Pure Storage with pure copy of strings?

--- python-components-intern.el
+++ python-components-intern.el
@@ -394,47 +394,47 @@ Don't save anything for STR matching `py-input-filter-re' "
 
 (make-obsolete 'jpython-mode 'jython-mode nil)
 
-(push (purecopy "*Python*")  same-window-buffer-names)
-(push (purecopy "*IPython*")  same-window-buffer-names)
-
-(push (cons (purecopy "\\.py\\'")  'python-mode)  auto-mode-alist)
+;; (push "*Python*"  same-window-buffer-names)
+;; (push "*IPython*"  same-window-buffer-names)
 
 ;; Python Macro File
+(unless (member '("\\.py\\'" . python-mode) auto-mode-alist)
+  (push (cons "\\.py\\'"  'python-mode)  auto-mode-alist))
 
-(unless (member '(".pym'" . python-mode) auto-mode-alist)
-  (push (cons (purecopy "\\.pym\\'")  'python-mode)  auto-mode-alist))
+(unless (member '("\\.pym\\'" . python-mode) auto-mode-alist)
+  (push (cons "\\.pym\\'"  'python-mode)  auto-mode-alist))
 
-(unless (member '(".pyc'" . python-mode)  auto-mode-alist)
-  (push (cons (purecopy "\\.pyc\\'")  'python-mode)  auto-mode-alist))
+(unless (member '("\\.pyc\\'" . python-mode)  auto-mode-alist)
+  (push (cons "\\.pyc\\'"  'python-mode)  auto-mode-alist))
 
 ;; Pyrex Source
-(unless (member '(".pyx'" . python-mode)  auto-mode-alist)
-  (push (cons (purecopy "\\.pyx\\'")  'python-mode) auto-mode-alist))
+(unless (member '("\\.pyx\\'" . python-mode)  auto-mode-alist)
+  (push (cons "\\.pyx\\'"  'python-mode) auto-mode-alist))
 
 ;; Python Optimized Code
-(unless (member '(".pyo'" . python-mode)  auto-mode-alist)
-  (push (cons (purecopy "\\.pyo\\'")  'python-mode) auto-mode-alist))
+(unless (member '("\\.pyo\\'" . python-mode)  auto-mode-alist)
+  (push (cons "\\.pyo\\'"  'python-mode) auto-mode-alist))
 
 ;; Pyrex Definition File
-(unless (member '(".pxd'" . python-mode)  auto-mode-alist)
-  (push (cons (purecopy "\\.pxd\\'")  'python-mode) auto-mode-alist))
+(unless (member '("\\.pxd\\'" . python-mode)  auto-mode-alist)
+  (push (cons "\\.pxd\\'"  'python-mode) auto-mode-alist))
 
 ;; Python Repository
-(unless (member '(".pyr'" . python-mode)  auto-mode-alist)
-  (push (cons (purecopy "\\.pyr\\'")  'python-mode)  auto-mode-alist))
+(unless (member '("\\.pyr\\'" . python-mode)  auto-mode-alist)
+  (push (cons "\\.pyr\\'"  'python-mode)  auto-mode-alist))
 
 ;; Python Stub file
 ;; https://www.python.org/dev/peps/pep-0484/#stub-files
-(unless (member '(".pyi'" . python-mode)  auto-mode-alist)
-  (push (cons (purecopy "\\.pyi\\'")  'python-mode)  auto-mode-alist))
+(unless (member '("\\.pyi\\'" . python-mode)  auto-mode-alist)
+  (push (cons "\\.pyi\\'"  'python-mode)  auto-mode-alist))
 
 ;; Python Path Configuration
-(unless (member '(".pth'" . python-mode)  auto-mode-alist)
-  (push (cons (purecopy "\\.pth\\'")  'python-mode)  auto-mode-alist))
+(unless (member '("\\.pth\\'" . python-mode)  auto-mode-alist)
+  (push (cons "\\.pth\\'"  'python-mode)  auto-mode-alist))
 
 ;; Python Wheels
-(unless (member '(".whl'" . python-mode)  auto-mode-alist)
-  (push (cons (purecopy "\\.whl\\'")  'python-mode)  auto-mode-alist))
+(unless (member '("\\.whl\\'" . python-mode)  auto-mode-alist)
+  (push (cons "\\.whl\\'"  'python-mode)  auto-mode-alist))
 
 (unless (member '("!#[          ]*/.*[jp]ython[0-9.]*" . python-mode) magic-mode-alist)
   (push '("!#[ \\t]*/.*[jp]ython[0-9.]*" . python-mode) magic-mode-alist))
